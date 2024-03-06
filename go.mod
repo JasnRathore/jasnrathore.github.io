982d4928adde046c22452ded9bcbc4bb8290b3c1
KIllermarine <paradisegamer98@gmail.com>
2024-03-06 21:08:16 +0530

    added severless compatibility

---
+++ go.mod
@@ -0,0 +1,9 @@
+module main.go
+
+go 1.20
+
+require (
+	github.com/apex/gateway/v2 v2.0.0 // indirect
+	github.com/aws/aws-lambda-go v1.17.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,28 +1,33 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
-)
 
-type person struct {
-	FirstName string
-	LastName  string
-}
+	"github.com/apex/gateway"
+)
 
 func main() {
 	fmt.Println("Server Started")
+	port := flag.Int("port", -1, "specify a port")
+	flag.Parse()
 	//http.HandleFunc("/assets/", func(w http.ResponseWriter, r *http.Request) {
 	//	http.Error(w, "Access denied", http.StatusForbidden)
 	//})
+	listener := gateway.ListenAndServe
+	portStr := "n/a"
+	if *port != -1 {
+		portStr = fmt.Sprintf(":%d", *port)
+		listener = http.ListenAndServe
+		http.Handle("/", http.FileServer(http.Dir("./public")))
+		http.Handle("/portfolio", http.RedirectHandler("https://github.com/JasnRathore/portfolio", http.StatusSeeOther))
+		http.Handle("/hades", http.RedirectHandler("https://github.com/JasnRathore/Hades", http.StatusSeeOther))
+		http.Handle("/github", http.RedirectHandler("https://github.com/JasnRathore", http.StatusSeeOther))
+		http.Handle("/linkedin", http.RedirectHandler("https://www.linkedin.com/in/jasn-rathore-884644256", http.StatusSeeOther))
+		http.Handle("/embed", http.RedirectHandler("https://astolfo.co", http.StatusSeeOther))
+	}
 
-	http.Handle("/", http.FileServer(http.Dir("./public")))
-	http.Handle("/portfolio", http.RedirectHandler("https://github.com/JasnRathore/portfolio", http.StatusSeeOther))
-	http.Handle("/hades", http.RedirectHandler("https://github.com/JasnRathore/Hades", http.StatusSeeOther))
-	http.Handle("/github", http.RedirectHandler("https://github.com/JasnRathore", http.StatusSeeOther))
-	http.Handle("/linkedin", http.RedirectHandler("https://www.linkedin.com/in/jasn-rathore-884644256", http.StatusSeeOther))
-	http.Handle("/embed", http.RedirectHandler("https://astolfo.co", http.StatusSeeOther))
-
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	log.Fatal(listener(portStr, nil))
 }
